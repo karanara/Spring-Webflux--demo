080f67d99d91cee50cd509561c607204d469873b
karanara <karanara@mail.uc.edu>
2024-02-25 01:33:00 -0500

    added OPENAPI3 configuration for swagger

--- src/main/java/com/example/webflux/demo/springreactive/SpringreactiveApplication.java
+++ src/main/java/com/example/webflux/demo/springreactive/SpringreactiveApplication.java
@@ -3,15 +3,16 @@ package com.example.webflux.demo.springreactive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-//import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 
 @SpringBootApplication
-/*@OpenAPIDefinition(info=@Info(
+@OpenAPIDefinition(info=@Info(
 		title="Spring webflux Crud Example without MongoDB",
 		version="2.0",
 		description="sample program for showcasing webflux concepts"
-		))*/
+		))
 public class SpringreactiveApplication {
 
 	public static void main(String[] args) {
